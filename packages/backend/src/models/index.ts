8198427dffc387b10af48594691cbb5d48bcd6f2
Mjas-254 <jamesnganga6999@gmail.com>
2026-07-25 07:35:16 +0300

    Add packages/backend/src/models/index.ts

---
+++ packages/backend/src/models/index.ts
@@ -0,0 +1,6 @@
+export { User, type IUser, type UserRole } from "./User.js";
+export { Farm, type IFarm } from "./Farm.js";
+export { Harvest, type IHarvest, type HarvestStatus } from "./Harvest.js";
+export { Reward, type IReward, type RewardStatus } from "./Reward.js";
+export { Redemption, type IRedemption, type RedemptionMethod, type RedemptionStatus } from "./Redemption.js";
+export { LearningProgress, type ILearningProgress } from "./LearningProgress.js";
